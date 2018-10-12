6045c03be82a4276ba853d1ad332c496caaf8eb4
Brandon Hinshaw <bkhinshaw@gmail.com>
2018-10-12 02:09:00 -0700

    add redux devtools support

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { reducer } from "redux-form";
 import thunk from 'redux-thunk';
 import peopleReducer from "./reducers/people";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 
 export default createStore(
   combineReducers({
     form: reducer,
     people: peopleReducer 
   }),
-  applyMiddleware(thunk)
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 );
